7412801c244e4fef6b42ae4d1d5c9d4deaae2c55
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-08-03 23:55:15 +0200

    clru tests

--- tests/lru/lru_test.cpp
+++ tests/lru/lru_test.cpp
@@ -12,12 +12,12 @@
 // Test fixture for LRU with string keys
 class LRUStringTest : public ::testing::Test
 {
-  protected:
+  public:
     LRU<std::string> cache;
 
-    LRUStringTest() : cache() {}
+    LRUStringTest() = default;
 
-    std::shared_ptr<PrimitiveNode<std::string>> make_node(
+    static std::shared_ptr<PrimitiveNode<std::string>> make_node(
             const std::string& key)
     {
         return std::make_shared<PrimitiveNode<std::string>>(key);
@@ -54,4 +54,29 @@ TEST_F(LRUStringTest, PutMultipleNodesUpdatesHead)
     auto head = cache.get_head();
     ASSERT_NE(head, nullptr);
     EXPECT_EQ(head->get_key(), "two"); // Most recently added should be head
+}
+
+TEST_F(LRUStringTest, PutMultipleNodesUpdatesHeadAndExistingOne)
+{
+    auto node1 = make_node("one");
+    auto node2 = make_node("two");
+    auto node3 = make_node("three");
+    auto node4 = make_node("four");
+
+    cache.put(node1);
+    cache.put(node2);
+
+    auto head = cache.get_head();
+    ASSERT_NE(head, nullptr);
+    EXPECT_EQ(head->get_key(), "two"); // Most recently added should be head
+
+    cache.put(node3);
+    auto head1 = cache.get_head();
+    ASSERT_NE(head1, nullptr);
+    EXPECT_EQ(head1->get_key(), "three"); // Most recently added should be head
+
+    cache.put(node1);
+    auto head2 = cache.get_head();
+    ASSERT_NE(head2, nullptr);
+    EXPECT_EQ(head2->get_key(), "one"); // Most recently added should be head
 }

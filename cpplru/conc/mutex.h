2f7e075adbd4457767330be87a35819a39ce5930
Isai GORDEEV <i.gordeev@groupeonepoint.com>
2025-08-05 23:46:07 +0200

    simple mutex

---
+++ cpplru/conc/mutex.h
@@ -0,0 +1,44 @@
+//
+// Created by Isai GORDEEV on 05/08/2025.
+//
+#pragma once
+
+#include <atomic>
+#include <thread>
+
+class Mutex {
+public:
+    void Lock() {
+        uint32_t expected = 0;
+
+        if (locked_.compare_exchange_strong(expected, 1, std::memory_order_acquire)) {
+            return;
+        }
+
+        while (true) {
+            expected = 0;
+            if (locked_.load(std::memory_order_relaxed) == 0) {
+                if (locked_.compare_exchange_strong(expected, 1, std::memory_order_acquire)) {
+                    return;
+                }
+            }
+            std::this_thread::yield(); // fallback when waiting, as std::futex is not standard yet
+        }
+    }
+
+    void Unlock() {
+        locked_.store(0, std::memory_order_release);
+    }
+
+    // BasicLockable
+    void lock() {
+        Lock();
+    }
+
+    void unlock() {
+        Unlock();
+    }
+
+private:
+    std::atomic_uint32_t locked_{0};
+};

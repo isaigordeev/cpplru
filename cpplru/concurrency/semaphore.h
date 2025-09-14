9fcaac464f8380a507b21bdb9755de5a5d168473
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2025-09-14 19:41:33 +0200

    abst semaphore + spinsemaphore

--- cpplru/concurrency/semaphore.h
+++ cpplru/concurrency/semaphore.h
@@ -1,22 +1,28 @@
 #pragma once
 #include <condition_variable>
 #include <mutex>
+#include <thread>
 
-class PrimitiveSemaphore
-{
+template <typename Derived>
+class SemaphoreBase {
  public:
-  explicit PrimitiveSemaphore(size_t permits = 0) : permits_(permits){};
+  void Acquire() { static_cast<Derived*>(this)->AcquireImpl(); };
 
-  void Acquire()
-  {
+  void Release() { static_cast<Derived*>(this)->ReleaseImpl(); };
+};
+
+class PrimitiveClassicSemaphore {
+ public:
+  explicit PrimitiveClassicSemaphore(size_t permits = 0) : permits_(permits){};
+
+  void AcquireImpl() {
     std::unique_lock<std::mutex> lock(mutex_);
     cv_.wait(lock, [this]() { return permits_ > 0; });
 
     --permits_;
   }
 
-  void Release()
-  {
+  void ReleaseImpl() {
     std::unique_lock<std::mutex> lock(mutex_);
     ++permits_;
     cv_.notify_one();
@@ -26,4 +32,24 @@ class PrimitiveSemaphore
   std::mutex mutex_;
   size_t permits_;
   std::condition_variable cv_;
+};
+
+class PrimitiveSpinSemaphore : SemaphoreBase<PrimitiveSpinSemaphore> {
+ public:
+  void AcquireImpl() {
+    size_t expected;
+
+    while (true) {
+      expected = permits_.load(std::memory_order_seq_cst);
+      if (expected && permits_.compare_exchange_weak(
+                          expected, expected - 1, std::memory_order_seq_cst)) {
+        return;
+      }
+      std::this_thread::yield();
+    }
+  };
+  void ReleaseImpl() { permits_.fetch_add(1, std::memory_order_seq_cst); };
+
+ private:
+  std::atomic<size_t> permits_;
 };
